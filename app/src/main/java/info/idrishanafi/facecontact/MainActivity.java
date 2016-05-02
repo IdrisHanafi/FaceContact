bfbc540a7c2ab32a9da6a26dacf3a4fadcf0111f
Idris Hanafi <ifh101@gmail.com>
2016-05-02 18:42:08 -0400

    Face Detecting Working

---
+++ app/src/main/java/info/idrishanafi/facecontact/MainActivity.java
@@ -0,0 +1,30 @@
+package info.idrishanafi.facecontact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FaceOverlayView mFaceOverlayView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mFaceOverlayView = (FaceOverlayView) findViewById( R.id.face_overlay );
+
+        InputStream stream = getResources().openRawResource( R.raw.face );
+        Bitmap bitmap = BitmapFactory.decodeStream(stream);
+
+        mFaceOverlayView.setBitmap(bitmap);
+
+    }
+}
